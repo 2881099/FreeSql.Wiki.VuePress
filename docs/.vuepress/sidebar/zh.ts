81417faa67b3910f023f0b430f2c60954ff078e9
2881099 <2881099@users.noreply.github.com>
2022-09-09 00:08:34 +0800

    Update zh.ts

--- docs/.vuepress/sidebar/zh.ts
+++ docs/.vuepress/sidebar/zh.ts
@@ -62,11 +62,11 @@ export const zhSidebarConfig = sidebar({
                     collapsable: true,
                     children: [
                         "/guide/freesql-provider-custom.md",
-                        "/guide/freesql-provider-odbc.md",
                         "/guide/freesql-provider-sqlitecore.md",
                         "/guide/freesql-provider-mysqlconnector.md",
                         "/guide/freesql-provider-oracle.md",
                         // "/guide/freesql-provider-postgresql.md",
+                        "/guide/freesql-provider-odbc.md",
                     ],
                 },
                 {
