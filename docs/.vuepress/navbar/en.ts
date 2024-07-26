1175b86d1d411ca3a6c4920b9e34432c892d2964
2881099 <2881099@qq.com>
2024-07-26 17:25:19 +0800

    1

--- docs/.vuepress/navbar/en.ts
+++ docs/.vuepress/navbar/en.ts
@@ -5,4 +5,10 @@ export const enNavbarConfig = navbar([
     { text: 'NuGet', link: 'https://www.nuget.org/packages?q=freesql' },
     { text: 'API Browser', link: 'https://dotnetcore.github.io/FreeSql/api/index.html' },
     { text: 'Redis Client', link: 'https://www.github.com/2881099/FreeRedis' },
+    
+    {
+        text: '捐赠',
+        icon: "launch",
+        link: "/reference/service-support.md"
+    },
 ]);
